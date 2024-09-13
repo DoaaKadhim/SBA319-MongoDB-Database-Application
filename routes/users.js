836542e3194425f36de68883c64188c7ccc8f093
Doaa Kadhim <doaa.abdulwahed88@gmail.com>
2024-09-13 14:12:46 -0500

    11th commit

--- routes/users.js
+++ routes/users.js
@@ -3,41 +3,92 @@ import express from 'express';
 const router = express.Router();
 import User from '../models/user.js';
 
+// // GET all users
+// router.get('/', async (req, res) => {
+//   const users = await User.find();
+//   res.json(users);
+// });
+
+// // POST a new user
+// router.post('/', async (req, res) => {
+//   const newUser = new User(req.body);
+//   try {
+//     const savedUser = await newUser.save();
+//     res.status(201).json(savedUser);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
+
+// // PATCH (update) a user
+// router.patch('/:id', async (req, res) => {
+//   try {
+//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//     res.json(updatedUser);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
+
+// // DELETE a user
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     await User.findByIdAndDelete(req.params.id);
+//     res.json({ message: 'User deleted' });
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
+//////////////////////////////////////////////////////////////////
 // GET all users
-router.get('/', async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
-
-// POST a new user
-router.post('/', async (req, res) => {
-  const newUser = new User(req.body);
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// PATCH (update) a user
-router.patch('/:id', async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE a user
-router.delete('/:id', async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.json({ message: 'User deleted' });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
+router.get('/', (req, res) => {
+    res.render('users', { users });
+  });
+  
+  
+  // GET a single user by ID
+  router.get('/:id', (req, res) => {
+      // const user = users.find(u => u.id === parseInt(req.params.id));
+      const user= User.findById(req.params.id)
+      
+      if (user) {
+        // res.render('user', { user });
+        res.send(user).status(200);
+      } else {
+        res.status(404).render('error', { message: 'User not found' });
+      }
+    });
+  
+  // POST a new user
+  router.post('/',async (req, res) => {
+    const newUser = {
+    //   id: users.length + 1,
+      name: req.body.name,
+      email: req.body.email
+    };
+    const user=await User.create(newUser);
+    res.send(user);
+  });
+  
+  // PUT or PATCH - Update a user by ID
+  router.put('/:id', (req, res) => {
+      const user = users.find(u => u.id === parseInt(req.params.id));
+      if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        res.redirect('/users');
+      } else {
+        res.status(404).render('error', { message: 'User not found' });
+      }
+    });
+  
+  // DELETE a user
+  router.delete('/:id', (req, res) => {
+    // users = users.filter(u => u.id !== parseInt(req.params.id));
+    const query={_id:req.params.id};
+    User.deleteOne(query)
+    
+    res.redirect('/users');
+  });
+  
 export default router;
