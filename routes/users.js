5ba7ed29067d1e288191d7d0215b0c9fdf2e0408
Doaa Kadhim <doaa.abdulwahed88@gmail.com>
2024-09-16 20:30:18 -0500

    15th commit

--- routes/users.js
+++ routes/users.js
@@ -3,49 +3,6 @@ import express from 'express';
 const router = express.Router();
 import User from '../models/user.js';
 
-// // GET all users
-// router.get('/', async (req, res) => {
-//   const users = await User.find();
-//   res.json(users);
-// });
-
-// // POST a new user
-// router.post('/', async (req, res) => {
-//   const newUser = new User(req.body);
-//   try {
-//     const savedUser = await newUser.save();
-//     res.status(201).json(savedUser);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // PATCH (update) a user
-// router.patch('/:id', async (req, res) => {
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//     res.json(updatedUser);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// // DELETE a user
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.json({ message: 'User deleted' });
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-//////////////////////////////////////////////////////////////////
-// // GET all users this one work
-// router.get('/', (req, res) => {
-//     res.render('users', { users });
-//   });
-  
-  //////////////////try new one 
 
 // GET all users
 router.get('/', async (req, res) => {
